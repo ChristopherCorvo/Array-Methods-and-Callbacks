d35387cc66187e023676fc3f5190c22ffe3e5ace
Christopher Corvo <christophercorvo@gmail.com>
2020-09-15 16:46:19 -0400

    Complete Task3

--- index.js
+++ index.js
@@ -69,13 +69,16 @@ console.log(getFinals(fifaData));
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, 
 and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-
-    /* code here */
-
+function getYears(getfinals) {
+    let finalsResults = getFinals(fifaData)
+    let years = finalsResults.map(function(finalsResults){
+        return finalsResults.Year; 
+    });
+   
+    return years;
 };
 
-getYears();
+console.log(getYears(getFinals));
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
